9a755bbd86fbb89e8a6a88f24a648bb69e902903
Xxiance <142407971+Xxiance@users.noreply.github.com>
2025-11-01 08:09:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-
+while True:
+    try:
+        layers = int(input("How many layers on the tree? "))
+        for i in range(layers):
+            print(f"{' '*(layers-i)}{'*'*i}{'*'*(i-1)}")
+    except:
+        print("error")
